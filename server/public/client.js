f3e42ea41272be5eabf4ddff3edf745508819e52
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-03-15 19:01:50 -0500

    client.js: created addEquation to post input values to server, then runs getAllEquations (needs to be set up) on response and clears the input values

--- server/public/client.js
+++ server/public/client.js
@@ -4,4 +4,27 @@ $(document).ready(start);
 
 function start() {
     console.log('jq');
+    // $('#addition-btn').on('click', );
 }
+
+// function to post input values to server, on response, run function getAllEquations and empty input values
+function addEquation(){
+    let number1 = $('#first-number').val();
+    let number2 = $('#second-number').val();
+    $.ajax({
+        method: 'POST',
+        url: '/equations',
+        data: {
+            number1: number1,
+            number2: number2
+        }
+    }).then(function(response){
+        getAllEquations();
+        console.log('Woot! addEquation worked!');
+        number1 = $('#first-number').val('');
+        number2 = $('#second-number').val('');
+    }).catch(function(response){
+        console.log(`Whoops, this didn't work!`);
+        alert((`Whoops, this didn't work!`))
+    }) // end ajax post
+} // end addEquation
