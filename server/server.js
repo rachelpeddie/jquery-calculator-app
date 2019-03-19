90bfe78f6920a9c39f187c4b5f596fe150fda369
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-03-19 06:10:47 -0500

    changed port to allow deployment to heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('server/public'));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
